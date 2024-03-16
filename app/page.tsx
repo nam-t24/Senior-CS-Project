23c707748d31771006cd721b69705c9fb08c12b9
PC <bldsad888@gmail.com>
2024-03-16 04:14:39 -0500

    Add transitions to header and update header links

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default async function Index() {
       <div className="absolute top-0 w-screen 2xl:h-28 h-20 flex flex-row text-wrap">
         {/* Minted Logo */}
         <div className="2xl:pt-6 pt-4 2xl:pl-10 pl-6"><Link href="/" className={`${caveat.className} 2xl:text-6xl text-5xl text-black font-medium`}>Minted</Link></div>
-        {/* Headers (go nowhere) */}
+        {/* Header Tabs */}
         <div className="2xl:pl-40 pl-20 2xl:space-x-20 space-x-12 flex flex-1 text-2xl text-heading items-center">
-          <Link href="/">About</Link>
-          <Link href="/">How It Works</Link>
-          <Link href="/">Why Minted</Link>
+          <Link href="/about" className="hover:text-darkmaroon transition duration-300">About</Link>
+          <Link href="/how_it_works" className="hover:text-darkmaroon transition duration-300">How It Works</Link>
+          <Link href="/why_minted" className="hover:text-darkmaroon transition duration-300">Why Minted</Link>
         </div>
         {/* Log in/out Button */}
         <div className="flex flex-1 justify-end">
-          <button className="2xl:mt-6 mt-4 2xl:mr-10 mr-6 2xl:h-16 h-12 2xl:w-40 w-32 border-4 border-darkmaroon border-solid rounded-full">
-            <Link href="/login" className="2xl:text-xl text-lg text-darkmaroon font-semibold">Log In</Link>
+          <button className="2xl:mt-6 mt-4 2xl:mr-10 mr-6 2xl:h-16 h-12 2xl:w-40 w-32 border-4 border-darkmaroon border-solid rounded-full text-darkmaroon hover:bg-darkmaroon hover:text-primary transition duration-300">
+            <Link href="/login" className="2xl:text-xl text-lg font-semibold">Log In</Link>
           </button>
         </div>
       </div>
